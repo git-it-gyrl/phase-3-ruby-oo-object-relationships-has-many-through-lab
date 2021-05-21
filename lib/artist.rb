e8af2465bbe6a642649e93dc0fed25dbbeb159ca
celeste.tech@icloud.com <celeste.tech@icloud.com>
2021-05-21 13:30:53 -0500

    commit tree 99d20157a8ca5dbeaebb4b892b21d52e9676d0ad

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,31 @@
+require 'pry'
 
+class Artist
+    attr_reader :name 
+
+    @@all=[]
+
+    def initialize (name)
+        @name= name
+        @all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select do |song|
+            self== song.artist  
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def genres
+        self.songs.map do |song|
+            song.genre
+    end
+    
+end
